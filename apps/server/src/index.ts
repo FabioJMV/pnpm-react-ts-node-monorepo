a6d12b414f1c48576f3582713e9b1e941843e33d
FabioJMV <109473322+FabioJMV@users.noreply.github.com>
2024-05-23 23:23:37 -0400

    Fixing format and linting erros

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,15 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
 const fastifo = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
-fastifo.get('/', async (_request, reply) => {
-  await reply.send({ hello: 'world' })
-})
+fastifo.get("/", async (_request, reply) => {
+  await reply.send({ hello: "world" });
+});
 
-fastifo.listen({ port: 3000 }, function (err, _address) {
+fastifo.listen({ port: 3000 }, (err, _address) => {
   if (err !== null) {
-    fastifo.log.error(err)
+    fastifo.log.error(err);
   }
-  fastifo.log.info('server listening on PORT 2000')
-})
+  fastifo.log.info("server listening on PORT 2000");
+});
